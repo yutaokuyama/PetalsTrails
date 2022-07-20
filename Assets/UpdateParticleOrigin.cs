e8b3e0e8cf3fef34ea5e5d1d979db377e9736cf1
unknown <yutaokuyam@gmail.com>
2022-07-20 11:44:44 +0900

    Change particle emitter position by OSC

---
+++ Assets/UpdateParticleOrigin.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+using OscJack;
+
+namespace MultiLayerScreen
+{
+    public class UpdateParticleOrigin : MonoBehaviour
+    {
+        OscServer _server;
+        VisualEffect _particleEffect;
+        Vector3 _position;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _server = new OscServer(9000); // Port number
+            _particleEffect = this.GetComponent<VisualEffect>();
+            Debug.Log("Server listen on PORT 9000");
+
+            _server.MessageDispatcher.AddCallback(
+                "/test", // OSC address
+                (string address, OscDataHandle data) =>
+                {
+                    _position = new Vector3(data.GetElementAsFloat(0), data.GetElementAsFloat(1), data.GetElementAsFloat(2));
+                }
+            );
+
+        }
+        void updateParticlePosition(Vector3 position)
+        {
+            _particleEffect.SetVector3("ParticleOrigin", position);
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            _particleEffect.SetVector3("ParticleOrigin", _position);
+
+        }
+
+        void OnDestroy()
+        {
+            _server?.Dispose();
+            _server = null;
+        }
+    }
+}
