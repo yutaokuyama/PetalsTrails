60cf4f1f91e2732464e7eec789fc33f275b2a4f5
unknown <yutaokuyam@gmail.com>
2022-07-22 16:10:12 +0900

    Change Player potion based on screen ID

--- Assets/Scripts/LayeredScreenManager.cs
+++ Assets/Scripts/LayeredScreenManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using PrefsGUI;
 using Mirror;
+using OscJack;
+
 
 namespace LayeredScreen
 {
@@ -11,10 +13,36 @@ namespace LayeredScreen
         // Start is called before the first frame update
         [SyncVar]
         public float masterClock = 0.0f;
+
+        [SyncVar]
+        public int currentFlaggedScreenId = 0;
+
+
         public PrefsFloat offsetOfPC1 = new PrefsFloat("OffsetOfPC1");
         public PrefsInt OSCPort = new PrefsInt("OSCPort");
+
+        OscServer _server;
+        FlowDetector _flowDetector;
+
+        public Vector3 currentPlayerPosition = new Vector3(0.0f, 0.0f, 0.0f);
+
         void Start()
         {
+            startOSCServer();
+
+        }
+
+        void startOSCServer()
+        {
+            _server = new OscServer(OSCPort.Get()); // Port number
+            Debug.LogFormat("Server listen on PORT {0}", OSCPort.Get());
+            _server.MessageDispatcher.AddCallback(
+               "/point/piece", (string address, OscDataHandle data) =>
+    {
+        currentFlaggedScreenId = data.GetElementAsInt(0);
+/*        Debug.LogFormat("currentFlaggedScreenId:{0}", currentFlaggedScreenId);
+*/    }
+    );
         }
 
         // Update is called once per frame
